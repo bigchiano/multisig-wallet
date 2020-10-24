52fbed2a90ed21831745895c6f9d8c9d1addd0ca
Chris <pro@Chris-MacBook-Pro.local>
2020-10-24 18:54:38 +0100

    added revoke confirmation function

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,68 +1,165 @@
 pragma solidity >=0.4.22 <0.7.0;
 
 contract MultiSigWallet {
-    event Deposite(address indexed sender, uint amount, uint balance);
-    event SubmitTransaction(address indexed owner, uint indexed txIndex, address indexed to, uint value, bytes data);
-    event ConfirmTransaction(address indexed owner, uint  indexed txIndex);
-    event RevokeConfirmation(address indexed owner, uint  indexed txIndex);
-    event ExecuteTransaction(address indexed owner, uint  indexed txIndex);
-    
+    event Deposit(address indexed sender, uint256 amount, uint256 balance);
+    event SubmitTransaction(
+        address indexed owner,
+        uint256 indexed txIndex,
+        address indexed to,
+        uint256 value,
+        bytes data
+    );
+    event ConfirmTransaction(address indexed owner, uint256 indexed txIndex);
+    event RevokeConfirmation(address indexed owner, uint256 indexed txIndex);
+    event ExecuteTransaction(address indexed owner, uint256 indexed txIndex);
+
     address[] public owners;
     mapping(address => bool) public isOwner;
-    uint public numConfirmationsRequired;
-    
+    uint256 public numConfirmationsRequired;
+
     struct Transaction {
         address to;
-        uint value;
+        uint256 value;
         bytes data;
         bool executed;
         mapping(address => bool) isConfirmed;
-        uint numConfirmations;
+        uint256 numConfirmations;
     }
-    
+
     Transaction[] public transactions;
-    
-    constructor(address[] memory _owners, uint _numConfirmationsRequired) public {
+
+    constructor(address[] memory _owners, uint256 _numConfirmationsRequired)
+        public
+    {
         require(_owners.length > 0, "Owners required");
-        require(_numConfirmationsRequired > 0 &&  _numConfirmationsRequired <= _owners.length, "Invalid number of required confirmations");
-        
-        for (uint i = 0;  i < _owners.length; i++) {
+        require(
+            _numConfirmationsRequired > 0 &&
+                _numConfirmationsRequired <= _owners.length,
+            "Invalid number of required confirmations"
+        );
+
+        for (uint256 i = 0; i < _owners.length; i++) {
             address owner = _owners[i];
-            
+
             require(owner != address(0), "Invalid owner");
             require(!isOwner[owner], "Owner not unique");
-            
+
             isOwner[owner] = true;
             owners.push(owner);
         }
-        
+
         numConfirmationsRequired = _numConfirmationsRequired;
     }
-    
+
+    fallback() external payable {
+        emit Deposit(msg.sender, msg.value, address(this).balance);
+    }
+
+    function receive() external payable {
+        emit Deposit(msg.sender, msg.value, address(this).balance);
+    }
+
     modifier onlyOwner() {
         require(isOwner[msg.sender], "Not owner");
         _;
     }
-    
-    function submitTransaction(address _to, uint _value, bytes memory _data) public onlyOwner {
-        uint txIndex = transactions.length;
-        
-        transactions.push(Transaction({
-            to: _to,
-            value: _value,
-            data: _data,
-            executed:  false,
-            numConfirmations: 0
-        }));
-        
+
+    function submitTransaction(
+        address _to,
+        uint256 _value,
+        bytes memory _data
+    ) public onlyOwner {
+        uint256 txIndex = transactions.length;
+
+        transactions.push(
+            Transaction({
+                to: _to,
+                value: _value,
+                data: _data,
+                executed: false,
+                numConfirmations: 0
+            })
+        );
+
         emit SubmitTransaction(msg.sender, txIndex, _to, _value, _data);
     }
-    
-    function confirmTransaction(uint _txIndex) public onlyOwner txExists(_txIndex) notExecuted(_txIndex) notConfirmed(_txIndex) {
-        
+
+    modifier txExists(uint256 _txIndex) {
+        require(_txIndex < transactions.length, "Tx does not exists");
+        _;
     }
-    
-    function executeTransaction() {}
-    
-    function revokeConfirmation() {}
+
+    modifier notExecuted(uint256 _txIndex) {
+        require(!transactions[_txIndex].executed, "Tx already executed");
+        _;
+    }
+
+    modifier confirmed(uint256 _txIndex) {
+        require(
+            transactions[_txIndex].isConfirmed[msg.sender],
+            "You have not confirmed tx"
+        );
+        _;
+    }
+
+    modifier notConfirmed(uint256 _txIndex) {
+        require(
+            !transactions[_txIndex].isConfirmed[msg.sender],
+            "You already confirmed tx"
+        );
+        _;
+    }
+
+    function confirmTransaction(uint256 _txIndex)
+        public
+        onlyOwner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+        notConfirmed(_txIndex)
+    {
+        Transaction storage transaction = transactions[_txIndex];
+
+        transaction.isConfirmed[msg.sender] = true;
+        transaction.numConfirmations += 1;
+
+        emit ConfirmTransaction(msg.sender, _txIndex);
+    }
+
+    function executeTransaction(uint256 _txIndex)
+        public
+        onlyOwner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+    {
+        Transaction storage transaction = transactions[_txIndex];
+
+        require(
+            transaction.numConfirmations >= numConfirmationsRequired,
+            "Cannot execute tx"
+        );
+
+        transaction.executed = true;
+
+        (bool success, ) = transaction.to.call{value: transaction.value}(
+            transaction.data
+        );
+        require(success, "Tx failed");
+
+        emit ExecuteTransaction(msg.sender, _txIndex);
+    }
+
+    function revokeConfirmation(uint256 _txIndex)
+        public
+        onlyOwner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+        confirmed(_txIndex)
+    {
+        Transaction storage transaction = transactions[_txIndex];
+
+        transaction.isConfirmed[msg.sender] = false;
+        transaction.numConfirmations -= 1;
+
+        emit RevokeConfirmation(msg.sender, _txIndex);
+    }
 }
