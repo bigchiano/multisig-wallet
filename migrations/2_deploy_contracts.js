7b327154daacd252907f59301d593bd5a42451dc
Chris <pro@Chris-MacBook-Pro.local>
2020-10-31 12:27:25 +0100

    added tests

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,8 @@
+const MultiSigWallet = artifacts.require("MultiSigWallet");
+
+module.exports = function (deployer, network, accounts) {
+  const owners = accounts.slice(0, 3)
+  const numConfirmationRequired = 2
+  
+  deployer.deploy(MultiSigWallet, owners, numConfirmationRequired);
+};
--- test/multi-sig-wallet.js
+++ test/multi-sig-wallet.js
@@ -1,2 +1,55 @@
-const chai = require('chain')
+const { assert, expect } = require('chai')
+const chai = require('chai')
 chai.use(require('chai-as-promised'))
+
+const MultiSigWallet = artifacts.require('MultiSigWallet')
+
+contract('MultiSigWallet', (accounts) => {
+  const owners = [accounts[0], accounts[1], accounts[2]]
+  const numOfConfirmationRequired = 2
+
+  let wallet
+  beforeEach(async () => {
+    wallet = await MultiSigWallet.new(owners, numOfConfirmationRequired)
+  })
+
+  describe('execteTransaction', () => {
+    beforeEach(async () => {
+      const to = owners[0]
+      const value = 0
+      const data = '0x0'
+
+      await wallet.submitTransaction(to, value, data, { from: owners[0] })
+      await wallet.confirmTransaction(0, { from: owners[0] })
+      await wallet.confirmTransaction(0, { from: owners[1] })
+    })
+
+    // execute transaction schould succeed
+    it('Should execute', async () => {
+      const res = await wallet.executeTransaction(0, { from: owners[0] })
+      const { logs } = res
+
+      assert.equal(logs[0].event, 'ExecuteTransaction')
+      assert.equal(logs[0].args.owner, owners[0])
+      assert.equal(logs[0].args.txIndex, 0)
+
+      const tx = await wallet.transactions(0)
+      assert.equal(tx.executed, true)
+    })
+
+    // execute transaction schould fail if transaction already executed
+    it('Should reject if already executed', async () => {
+      await wallet.executeTransaction(0, { from: owners[0] })
+
+      await expect(wallet.executeTransaction(0, { from: owners[0] })).to.be
+        .rejected
+
+      // try {
+      //     await wallet.executeTransaction(0, { from: owners[0] })
+      //     throw new Error('Tx did not fail')
+      // } catch (error) {
+      //     assert.equal(error.reason, 'Tx already executed')
+      // }
+    })
+  })
+})
